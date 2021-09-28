21c401c0789b79664ab4ba876ca46a419b879d66
Alex Gorelick <agorelick@mgh.harvard.edu>
2021-09-28 14:37:41 -0400

    adding martins error handling for bad input arguments

--- R/rds.R
+++ R/rds.R
@@ -61,6 +61,7 @@ multibinomial <- function(m, l, cluster_numbers) {
 ##' @return number of trees where the given clusters have independent origin
 ##' @export
 no_trees_with_clusters <- function(k, clusters){
+    require(gmp)
     n = k + sum(clusters)
     no_trees = 1
     for (c in clusters){
@@ -108,26 +109,27 @@ no_trees_min_cluster <- function(k, m, l){
 ##' @return probabilility that a cluster of size l arises by chance
 ##' @export
 min_cluster_probability <- function(k, m, l) {
+    if (l > m) {
+        stop("L is larger than M. Make sure KLM values are accurate to continue.")
+    }
     if (m == 1) {
-        NA }
-    else if (l == 1){
+        NA 
+    } else if (l == 1){
         1
     }  else {
-        # this is an addition to the python script to allow for RDS caculcations for over 10,000 samples      
-        k <- gmp::as.bigz(k)     
+        # this is an addition to the python script to allow for RDS caculcations for over 10,000 samples
+        k <- as.bigz(k)
         n = k + m
         no_trees = no_trees_min_cluster(k, m, l)
-
-        if (is.infinite(no_trees) | is.infinite(calculate_no_trees(n))) { 
+        if (is.infinite(no_trees) | is.infinite(calculate_no_trees(n))) {
             stop("The number of trees calculated are too large for R to handle. Reduce input KLM or change script.")
-        }   
-
-        # if the number of trees gets too large, R can't display it and will difine it as infinite.
-        # to adapt the script to work with R, the RDS  
+        }
+        # if the number of trees gets too large, R can’t display it and will difine it as infinite.
+        # to adapt the script to work with R, the RDS
         # uses the result from equation (S1) and implements equation (S2)
-        as.numeric(no_trees / calculate_no_trees(n)) 
+        as.numeric(no_trees / calculate_no_trees(n))
     }
-}   
+}
 
 
 ##' rds
